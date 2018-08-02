5e7aa00431aa0fb7e5f7be6e7843ed31f1c5b400
lauren-hart <laurenannehart@gmail.com>
2018-08-02 16:39:04 +1200

    background colour changes when you click on a colour

--- projects/painting/personal.js
+++ projects/painting/personal.js
@@ -1,12 +1,47 @@
 
+//When the DOM content has been loaded the start
+//function will run
 
+document.addEventListener('DOMContentLoaded', start)
 
+//The start function will run a function called
+//addEventListener
 
- $(document).ready(function(){
+function start() {
+    addEventListener()
+ }
+ 
+//The function addEventListener will then run
+//to add a click event to the buttons
 
-    $(".easel").mouseover(function(){
-        console.log(("paint"));
-    });
+ function addEventListener () {
 
+    var val = document.getElementsByTagName("p");
 
-});
+        for (var i = 0; i < val.length; i++) {
+            val[i].addEventListener("click", checkClass)
+        }
+    }
+
+function checkClass () {
+
+    if (this.className === 'blue') {
+        document.getElementById("easel").style.backgroundColor = "blue";
+
+    }  else if (this.className === 'green') {
+        document.getElementById("easel").style.backgroundColor = "rgb(21, 231, 21)";
+
+    }  else if (this.className === 'red') {
+        document.getElementById("easel").style.backgroundColor = "rgb(231, 46, 21)";
+
+    }  else if (this.className === 'purple') {
+        document.getElementById("easel").style.backgroundColor = "rgb(172, 18, 233)";
+
+    }  else if (this.className === 'pink') {
+        document.getElementById("easel").style.backgroundColor = "rgb(231, 21, 186)";
+
+    }  else if (this.className === 'yellow') {
+        document.getElementById("easel").style.backgroundColor = "yellow";
+    }
+
+}
