16911940c742e6488d9f5f9b04d09a6dbeed2056
lauren-hart <laurenannehart@gmail.com>
2018-08-02 21:02:00 +1200

    final changes, ready for upload

--- projects/painting/personal.js
+++ projects/painting/personal.js
@@ -7,9 +7,16 @@ document.addEventListener('DOMContentLoaded', start)
 //The start function will run a function called
 //addEventListener
 
+
+
+
+
 function start() {
     addEventListener()
+
  }
+
+
  
 //The function addEventListener will then run
 //to add a click event to the buttons
@@ -30,32 +37,54 @@ function checkId () {
 
     if (this.id === 'blue') {
 
+        document.getElementById("myAnimation").innerHTML = "GOT THE BLUES?";
+        document.getElementById("myAnimation").style.color = "blue";
         document.getElementById("easel").style.backgroundColor = 'blue';
         document.getElementById("header").style.backgroundColor = 'blue';
 
+
     }  else if (this.id === 'green') {
+        
+        document.getElementById("myAnimation").innerHTML = "ENVIOUS";
+        document.getElementById("myAnimation").style.color = "rgb(21, 231, 21)";
         document.getElementById("easel").style.backgroundColor = "rgb(21, 231, 21)";
         document.getElementById("header").style.backgroundColor = "rgb(21, 231, 21)";
 
     }  else if (this.id === 'red') {
+
+        document.getElementById("myAnimation").innerHTML = "REDDY?";
+        document.getElementById("myAnimation").style.color = "rgb(231, 46, 21)";
         document.getElementById("easel").style.backgroundColor = "rgb(231, 46, 21)";
         document.getElementById("header").style.backgroundColor = "rgb(231, 46, 21)";
 
     }  else if (this.id === 'purple') {
+
+        document.getElementById("myAnimation").innerHTML = "PURPLE RAIN";
+        document.getElementById("myAnimation").style.color = "rgb(172, 18, 233)";
         document.getElementById("easel").style.backgroundColor = "rgb(172, 18, 233)";
         document.getElementById("header").style.backgroundColor = "rgb(172, 18, 233)";
 
     }  else if (this.id === 'pink') {
+
+        document.getElementById("myAnimation").innerHTML = "PRETTY IN PINK!";
+        document.getElementById("myAnimation").style.color = "rgb(231, 21, 186)";
         document.getElementById("easel").style.backgroundColor = "rgb(231, 21, 186)";
         document.getElementById("header").style.backgroundColor = "rgb(231, 21, 186)";
 
     }  else if (this.id === 'yellow') {
+
+        document.getElementById("myAnimation").innerHTML = "SHINING BRIGHT";
+        document.getElementById("myAnimation").style.color = "yellow";
         document.getElementById("easel").style.backgroundColor = "yellow";
         document.getElementById("header").style.backgroundColor = "yellow";
 
     } else if (this.id === "eraser") {
+
+        document.getElementById("myAnimation").innerHTML = "ALL GONE!";
+        document.getElementById("myAnimation").style.color = "white";
         document.getElementById("easel").style.backgroundColor = "white";
         document.getElementById("header").style.backgroundColor = "white";
     }
 
 }
+
